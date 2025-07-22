7f284663424be25919de0b24fa2f3f365c5eefa6
michal.ziabka <michal.ziabka@gmail.com>
2025-07-22 14:42:33 +0200

    added tests

---
+++ BoldareApp.Tests/BreweryServiceTests.cs
@@ -0,0 +1,42 @@
+﻿using BoldareApp.Infrastructure.DbCache;
+using BoldareApp.Queries;
+using BoldareApp.Services;
+using FluentAssertions;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Caching.Memory;
+using Microsoft.Extensions.Logging;
+using Moq;
+
+namespace BoldareApp.Tests
+{
+    public class BreweryServiceTests
+    {
+        //Example test
+
+        [Fact]
+        public async Task GetBreweriesAsync_ShouldReturnData()
+        {
+            var dbOptions = new DbContextOptionsBuilder<DbCacheContext>()
+           .UseInMemoryDatabase(databaseName: Guid.NewGuid().ToString())
+           .Options;
+
+            using var dbContext = new DbCacheContext(dbOptions);
+
+            // Arrange
+            var memoryCache = new MemoryCache(new MemoryCacheOptions());
+            var loggerMock = new Mock<ILogger<BreweryService>>();
+            var httpClient = new HttpClient(new FakeHttpMessageHandler());
+            var service = new BreweryService(httpClient, memoryCache, loggerMock.Object, dbContext);
+
+            var query = new FilterQuery { Name = "Test", City = "Katowice" };
+
+            // Act
+            var call = await service.GetBreweriesAsync(query);
+            var secondCall = await service.GetBreweriesAsync(query);
+
+            // Assert
+            Assert.NotNull(call);
+            call.Should().HaveCount(1);
+        }
+    }
+}
---
+++ BoldareApp.Tests/FakeHttpMessageHandler.cs
@@ -0,0 +1,26 @@
+﻿using BoldareApp.Models;
+using System.Net;
+using System.Text;
+using System.Text.Json;
+
+namespace BoldareApp.Tests
+{
+    public class FakeHttpMessageHandler : HttpMessageHandler
+    {
+        protected override Task<HttpResponseMessage> SendAsync(HttpRequestMessage request, CancellationToken cancellationToken)
+        {
+            var fakeData = new[]
+            {
+                new BreweryModel { Id = Guid.NewGuid().ToString(), Name = "Test", City = "Katowice", PhoneNumber = "123456" }
+            };
+
+            var json = JsonSerializer.Serialize(fakeData);
+            var response = new HttpResponseMessage(HttpStatusCode.OK)
+            {
+                Content = new StringContent(json, Encoding.UTF8, "application/json")
+            };
+
+            return Task.FromResult(response);
+        }
+    }
+}
